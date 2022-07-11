800d402e8d1907fdb4eb3d4be04bdc7145283fc6
James Rozsypal <jimmerxl@gmail.com>
2022-07-11 10:37:32 -0400

    Move logic for sliding images out of the iife into its own method slideImage

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+const slideImage = (index, direction) => {
+  const images = document.querySelector(".image-gallery").children;
+  const imageCount = images.length;
+  let i = index;
+
+  if (direction === 'left') {
+    i++;
+    if (i == imageCount) {
+      i = 0;
+    }
+  }
+
+  if (direction === 'right') {
+    if (i == 0) {
+      i = imageCount - 1;
+    } else {
+      i--;
+    }
+  }
+
+  for(let j = 0; j < imageCount; j++) {
+    images[j].classList.remove("visible");
+  }
+  images[i].classList.add("visible");
+
+  return i;
+}
+
 (() => {
   const images = document.querySelector(".image-gallery").children;
   const imageCount = images.length;
@@ -8,29 +36,10 @@
   let i = 0;
 
   btnLeft.addEventListener("click", () => {
-    i++;
-    if (i == imageCount) {
-      i = 0;
-    }
-
-    for(let j = 0; j < imageCount; j++) {
-      images[j].classList.remove("visible");
-    }
-    images[i].classList.add("visible");
+    i = slideImage(i, 'left');
   });
   
   btnRight.addEventListener("click", () => {
-    if (i == 0) {
-      i = imageCount - 1;
-    } else {
-      i--;
-    }
-
-    for(let j = 0; j < imageCount; j++) {
-      images[j].classList.remove("visible");
-    }
-    images[i].classList.add("visible");
+    i = slideImage(i, 'right');
   });
-
-  
 })();
